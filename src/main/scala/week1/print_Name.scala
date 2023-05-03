a4f46af6da56d70a76b3417b8e2a2d872a45b1da
Parmeshwar <psalunke.terna@gmail.com>
2023-05-03 09:42:16 +0530

    Print Name 60 Times

---
+++ src/main/scala/week1/print_Name.scala
@@ -0,0 +1,15 @@
+package week1
+
+object print_Name {
+  def main(args: Array[String]): Unit = {
+
+    val a = 1 to 60
+    val i = 0
+    for (i <- a) {
+      println(s"SEEKHO BIGDATA   : $i")
+    }
+
+
+  }
+
+}
